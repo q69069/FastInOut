f378d60446d059ce61460110c4268a3995bdeef8
q69069 <q69069@users.noreply.github.com>
2026-05-02 14:53:49 +0800

    fix: H5 Layout.vue添加router-view显示内容区域

--- h5/src/views/Layout.vue
+++ h5/src/views/Layout.vue
@@ -1,11 +1,14 @@
 <template>
-  <van-tabbar v-model="active" route fixed placeholder safe-area-inset-bottom>
-    <van-tabbar-item to="/home" icon="home-o">首页</van-tabbar-item>
-    <van-tabbar-item to="/todo" icon="todo-o">待办</van-tabbar-item>
-    <van-tabbar-item to="/order" icon="orders-o">下单</van-tabbar-item>
-    <van-tabbar-item to="/inventory" icon="search">库存</van-tabbar-item>
-    <van-tabbar-item to="/performance" icon="chart-trending-o">业绩</van-tabbar-item>
-  </van-tabbar>
+  <div class="layout">
+    <router-view />
+    <van-tabbar v-model="active" route fixed placeholder safe-area-inset-bottom>
+      <van-tabbar-item to="/home" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/todo" icon="todo-o">待办</van-tabbar-item>
+      <van-tabbar-item to="/order" icon="orders-o">下单</van-tabbar-item>
+      <van-tabbar-item to="/inventory" icon="search">库存</van-tabbar-item>
+      <van-tabbar-item to="/performance" icon="chart-trending-o">业绩</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script setup>
@@ -13,3 +16,11 @@ import { ref } from 'vue'
 
 const active = ref(0)
 </script>
+
+<style>
+.layout {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
